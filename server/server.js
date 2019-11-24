7bf3109b3de376ac07cc63ee288c8b9d47716a19
ithgnm <ithgnm@gmail.com>
2019-11-25 01:27:39 +0700

    nodejs

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT || 7000
 
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb://'
+const uri = process.env.ATLAS_URI
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
 const connection = mongoose.connection
